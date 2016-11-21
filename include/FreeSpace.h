5f794401d43d56319e5be12fbc30bec628a0c0d3
latz1@gmx.net <latz1@gmx.net>
2016-11-21 17:34:51 +0100

    implemented next pointer getter and setter

--- include/FreeSpace.h
+++ include/FreeSpace.h
@@ -12,9 +12,20 @@
 class FreeSpace : public Space{
 private:
     bool copyNextPointerFromEndToFront(uint32_t* front, uint32_t* end);
+    uint32_t* getLeftNext(size_t codeBlockSize);
+    uint32_t* getRightNext(size_t codeBlockSize);
 
 public:
+    /**
+     * @return a pointer to the next FreeSpace in the bucket. The left next pointer will be taken as reference
+     */
     FreeSpace* getNext();
+
+    /**
+     * Adapt the next pointer in the data structure. The next pointer is adjacent to the code blocks and stored
+     * as a 4 byte integer interpreted as offset from the last byte (to the right)
+     * @param next pointer to the next free space
+     */
     void setNext(FreeSpace* next);
 
     /**
--- src/FreeSpace.cpp
+++ src/FreeSpace.cpp
@@ -13,8 +13,8 @@ FreeSpace *FreeSpace::resize(byte *firstByte) {
     if (firstByte == CodeBlock::getCodeBlock(firstByte, getRightMostEnd()-firstByte, codeBlockSize)){
         copyCodeBlocAtEnd(firstByte,codeBlockSize);
         copyNextPointerFromEndToFront(
-                (uint32_t*)(firstByte+codeBlockSize),
-                (uint32_t*)((getRightMostEnd()-(codeBlockSize-1))- sizeof(uint32_t))//uint32_t is 4 byte in contrast to the one byte rightMostEnd pointer
+                getLeftNext(codeBlockSize),
+                getRightNext(codeBlockSize-1)
                 );
         return (FreeSpace*)firstByte;
     }else{
@@ -27,4 +27,35 @@ FreeSpace *FreeSpace::resize(byte *firstByte) {
 bool FreeSpace::copyNextPointerFromEndToFront(uint32_t *front, uint32_t *end) {
     *front = *end;
     return true;
+}
+
+void FreeSpace::setNext(FreeSpace *next) {
+    size_t codeBlockSize = 0;
+    if (getLeftMostEnd() == CodeBlock::getCodeBlock(getLeftMostEnd(), getRightMostEnd()-getLeftMostEnd(), codeBlockSize)) {
+        uint32_t offset = (uint32_t) ((byte *) next - getRightMostEnd());
+        uint32_t *leftNext = getLeftNext(codeBlockSize);
+        uint32_t *rightNext = getRightNext(codeBlockSize);
+        *leftNext = offset;
+        *rightNext = offset;
+    }else{
+        Logger::error("Unable to build CodeBlock");
+    }
+}
+
+uint32_t *FreeSpace::getLeftNext(size_t codeBlockSize) {
+    return (uint32_t *) (getLeftMostEnd() + codeBlockSize);
+}
+
+uint32_t *FreeSpace::getRightNext(size_t codeBlockSize) {
+    return (uint32_t*)((getRightMostEnd()-(codeBlockSize-1))- sizeof(uint32_t));//uint32_t is 4 byte in contrast to the one byte rightMostEnd pointer
+}
+
+FreeSpace *FreeSpace::getNext() {
+    size_t codeBlockSize = 0;
+    if (getLeftMostEnd() == CodeBlock::getCodeBlock(getLeftMostEnd(), getRightMostEnd()-getLeftMostEnd(), codeBlockSize)) {
+        return (FreeSpace*)(getRightMostEnd()+(*getLeftNext(codeBlockSize)));
+    }else{
+        Logger::error("Unable to build CodeBlock");
+        return nullptr;
+    }
 }
