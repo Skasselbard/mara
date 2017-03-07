0293d9098969f9022b57b939efdbe72f934bf56e
Julian Gaede <jg367@uni-rostock.de>
2017-03-07 13:33:32 +0100

    inserted rng???

--- main.c
+++ main.c
@@ -2,25 +2,29 @@
 #include <memory.h>
 #include <time.h>
 
+unsigned int minSize = 4;
+unsigned int maxSize = 4000;
+unsigned int sizeSpan = maxSize - minSize;
+unsigned int requests = 10000;
+unsigned int seed = 1234;
+
 // usage: mara test minSize maxSize requests seed
 main(int argc, char **argv) {
     if (argc >= 6 && strcmp(argv[1], "test") == 0) {
-        unsigned int minSize = 4;
-        unsigned int maxSize = 4000;
-        unsigned int requests = 10000;
-        unsigned int seed = 1234;
+
         sscanf(argv[2], "%u", &minSize);
         sscanf(argv[3], "%u", &maxSize);
         sscanf(argv[4], "%u", &requests);
         sscanf(argv[5], "%u", &seed);
-
-        std::mt19937 generator(seed);
-        std::uniform_int_distribution<unsigned int> size_distr(minSize, maxSize);
+        
+        sizeSpan = maxSize - minSize;
+        
+        srand(seed);
 
         clock_t begin = clock();
 
         for (int i = 1; i <= requests; i++) {
-            unsigned int size = size_distr(generator);
+            unsigned int size = (rand() % sizeSpan) + 1;
             unsigned long *address;
 #ifdef USE_MARA
             address = (unsigned long *) staticNew(size);
