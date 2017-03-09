f6d54a94764bcc255e3fba1619175480bb50e739
Julian Gaede <julian.gaede@uni-rostock.de>
2017-03-09 18:22:29 +0100

    documented statistic

--- include/Statistic.h
+++ include/Statistic.h
@@ -10,14 +10,46 @@
 
 class Statistic {
 public:
+    /**
+     * Registers a new dynamic memory array into the statistic.
+     * @param size the array's size
+     * @param address the array's address
+     */
     static void newDynamic(size_t size, void * address);
+
+    /**
+     * Registers a new static memory array into the statistic.
+     * @param size the array's size
+     * @param address the array's address
+     */
     static void newStatic(size_t size, void * address);
 
+    /**
+     * "Unregisters" a dynamic memory array from the statistic.
+     * @param size the array's size
+     * @param address the array's address
+     */
     static void freeDynamic(size_t size, void * address);
 
+    /**
+     * Prints information about the used dynamic memory to stdout.
+     */
     static void logDynamic();
+
+    /**
+     * Prints information about the used static memory to stdout.
+     */
     static void logStatic();
+
+    /**
+     * Prints combined information about the used static and dynamic memory to stdout.
+     */
     static void logComplete();
+
+    /**
+     * Prints complete information over dynamic and static memory and codeblock size as a table.
+     * If STATISTIC_VERBOSE is set, also prints all used blocks (NOT RECOMMENDED FOR LARGE TESTS!!!)
+     */
     static void logTable();
 
     static unsigned int getUsedStaticMemory();
@@ -35,18 +67,49 @@ public:
     static unsigned int getDynamicBlocksPeak();
 
 private:
+    /**
+     * Static memory (in bytes) currently in use.
+     */
     static unsigned int usedStaticMemory;
+
+    /**
+     * Number of static blocks currently in use.
+     */
     static unsigned int usedStaticBlocks;
 
+    /**
+     * Dynamic memory (in bytes) currently in use, excluding codeblocks.
+     */
     static unsigned int usedDynamicMemory;
+
+    /**
+     * Number of dynamic blocks currently in use.
+     */
     static unsigned int usedDynamicBlocks;
+
+    /**
+     * Dynamic memory (in bytes) currently in use, including codeblocks.
+     */
     static unsigned int usedDynamicMemoryWithCodeblocks;
 
+    /**
+     * Maximum used dynamic memory since initialization in bytes.
+     */
     static unsigned int usedDynamicMemoryPeak;
+
+    /**
+     * Maximum used dynamic blocks since initialization.
+     */
     static unsigned int usedDynamicBlocksPeak;
 
-
+    /**
+     * Map of all dynamic blocks and their size. Only used for STATISTIC_VERBOSE.
+     */
     static std::map<void *, size_t> dynamicMemoryMap;
+
+    /**
+     * Map of all static blocks and their size. Only used for STATISTIC_VERBOSE.
+     */
     static std::map<void *, size_t> staticMemoryMap;
 };
 #endif //MARA_STATISTIC_H
