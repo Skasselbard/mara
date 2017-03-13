629be8e5f042a004db610f5bb488afd296ff309d
Julian Gaede <julian.gaede@uni-rostock.de>
2017-03-13 23:07:42 +0100

    init tester

---
+++ tester.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+echo "[1]	./mara.sh -i 50 -s 500 -r 10000000 -n 4 -x 1000"
+echo "[2]	iterate minSize"
+echo "[3]	iterate maxSize"
+echo "[4]	iterate pageSize"
+echo "[5]	iterate all"
