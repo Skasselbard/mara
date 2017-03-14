6f4304cf10151ac310c783541a4de4da11e4def9
Marian Stein <marian.stein@uni-rostock.de>
2017-03-14 13:21:19 +0100

    doxygen and readme

--- include/Mara.h
+++ include/Mara.h
@@ -27,8 +27,8 @@ public:
      * allocated with this function CANNOT be freed. Mara returns a pointer to the location with an unused block with the
      * given size and completely ignore this space in the future. The advantage is that these blocks will produce absolutely
      * overhead inside of its page.
-     * @param sizeInByte size of the block you want to use
-     * @return a pointer to the first byte of the block you want to use. After this operation the block will stay allocated
+     * \param sizeInByte size of the block you want to use
+     * \return a pointer to the first byte of the block you want to use. After this operation the block will stay allocated
      * until complete program termination.
      */
     void *staticNew(size_t sizeInBytes);
@@ -37,8 +37,8 @@ public:
      * Sets the default size for blocks allocated from the system. These blocks are used bei mara for internal memory management.
      * Calling staticNew and dynamicNew will allocate memory in one of these blocks. Because of internal structure, the
      * blocks cannot be greater than approximately 4GB.
-     * @param sizeInByte default size for malloc calls from mara
-     * @return 0 on success
+     * \param sizeInByte default size for malloc calls from mara
+     * \return 0 on success
      */
     int setPageSize(size_t sizeInByte);
 
