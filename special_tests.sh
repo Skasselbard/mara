47a51598aa14939c4361682effd4ce039bf6a132
Julian Gaede <jg367@uni-rostock.de>
2017-03-24 18:28:20 +0100

    created special tests hopefully they work

---
+++ special_tests.sh
@@ -0,0 +1,60 @@
+#!/bin/bash
+
+# Pagesizes: 128,256,512,1024,2048,4096 Mebibytes
+pagesizes=( 134217728 268435456 536870912 1073741824 2147483648 4294967296 )
+
+COMMON_ARGS="-m -i 50 -s 500 -r 50000000"
+
+function logsToCsv {
+    logFile=${newLogs}/${newLogs}.csv
+    touch ${logFile}
+    for l in ${newLogs}/*-eval.log
+    do
+        read -r requests minSize maxSize maxSeed pageSize < "${l}"
+        i=0
+	line=`grep "mara" ${l}`
+	echo $line
+        for number in ${line}
+        do
+            echo $number
+            case $i in
+                1)
+                total=$number
+                ;;
+                2)
+                factor=$number
+                ;;
+                *)
+                ;;
+            esac
+            i=$((i+1))
+        done
+        if [ -n $requests ] && [ -n $minSize ] && [ -n $maxSize ] && [ -n $maxSeed ] && [ -n $pageSize ]
+        then
+            requests=${requests#requests=}
+            minSize=${minSize#minSize=}
+            maxSize=${maxSize#maxSize=}
+            pageSize=${pageSize#pageSize=}
+            echo "${requests},${minSize},${maxSize},${pageSize},${total},${factor}" >> ${logFile}
+        fi
+    done
+}
+
+
+mv testlogs testlogs.bak
+newLogs=testlogs_`date +%Y-%m-%d_%H-%M-%S`
+
+for s in ${pagesizes[*]}
+do
+./mara.sh ${COMMON_ARGS} -n 4 -x 1000 -p ${s}
+./mara.sh ${COMMON_ARGS} -n 4 -x 32 -p ${s}
+./mara.sh ${COMMON_ARGS} -n 1000 -x 4000 -p ${s}
+./mara.sh ${COMMON_ARGS} -n 4 -x 4000 -p ${s}
+./mara.sh ${COMMON_ARGS} -n 100 -x 500 -p ${s}
+done
+
+mv testlogs ${newLogs}
+mkdir testlogs
+mv testlogs.bak/* testlogs
+
+logsToCsv
